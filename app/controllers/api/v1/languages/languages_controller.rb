8acd130c62b7c272bb41e1e17940148d20b83a81
Mike Heft <mikeheft@gmail.com>
2020-04-03 07:42:10 -0600

    Add LanguagesController

---
+++ app/controllers/api/v1/languages/languages_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Languages::LanguagesController < ApiController
+  
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :languages, only: :index
+      resources :languages, module: :languages, only: :index
     end
   end
 end
