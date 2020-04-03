896fe0c7f393a95b3769ec5fcbc946fce39de50a
Mike Heft <mikeheft@gmail.com>
2020-04-03 08:40:05 -0600

    Add method to update Language#totalBytes

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,12 @@
 class Language < ApplicationRecord
   
-  validates :name, presence: true, uniqueness: true
+  validates :name, :total_bytes, presence: true, uniqueness: true
+  validates :total_bytes, numericality: { greater_than_or_equal_to: 0 }
+  
+  def update_total_bytes(bytes)
+    new_total_bytes = total_bytes + bytes
+    
+    update(total_bytes: new_total_bytes)
+  end
+  
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Language, type: :model do
   context :validations do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
+    it {should validate_numericality_of(:total_bytes).is_greater_than_or_equal_to(0)}
   end
 end
