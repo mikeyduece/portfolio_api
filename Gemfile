fd43a704ef621c678e99652fe4fd1c05be36f712
Mike Heft <mikeheft@gmail.com>
2020-04-03 15:26:34 -0600

    Add sendgrid

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'faraday'
 gem 'figaro'
 gem 'codecov'
 gem 'sidekiq'
+gem 'sendgrid-ruby'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/api/v1/contact/contacts_controller.rb
+++ app/controllers/api/v1/contact/contacts_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Contact::ContactsController < ApiController
   
   def create
-    ContactMeMailer.contact_me(params).deliver_later
+    ContactMeMailer.contact_me(contact_params).deliver_later
+    render json: true
+  end
+  
+  private
+  
+  def contact_params
+    params.require(:contact).permit(:body, :name, :subject, :email, :contact)
   end
 end
