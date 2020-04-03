978f8463c15cf87ad761f5ce00071bbebefc2dff
Mike Heft <mikeheft@gmail.com>
2020-04-03 10:26:15 -0600

    Refactor specs to account for columns changes

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Repo, type: :model do
   context :validations do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
-    it { should validate_presence_of(:uuid) }
-    it { should validate_uniqueness_of(:uuid) }
+    it { should validate_presence_of(:github_id) }
+    it { should validate_uniqueness_of(:github_id) }
   end
 end
