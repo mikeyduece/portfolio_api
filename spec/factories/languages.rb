75b289f9a40fe6b330a599aa7fee5432fb7066fd
Mike Heft <mikeheft@gmail.com>
2020-04-07 12:22:18 -0600

    Refactor test to account for new json return

--- spec/factories/languages.rb
+++ spec/factories/languages.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :language do
     sequence :name do |n|
       name = %w[Ruby JavaScript Swift HTML CSS].sample
-      "#{n}" + name
+      name
     end
     total_bytes {rand(1..10_000)}
   end
