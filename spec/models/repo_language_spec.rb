9c39bf59f5c273d407ca892d8ef93596579aaf06
Mike Heft <mikeheft@gmail.com>
2020-04-12 11:36:38 -0600

    Remove unique validation from spec

--- spec/models/repo_language_spec.rb
+++ spec/models/repo_language_spec.rb
@@ -11,6 +11,5 @@ RSpec.describe RepoLanguage, type: :model do
   context :validations do
     it { should validate_presence_of(:repo_id) }
     it { should validate_presence_of(:language_id) }
-    it { should validate_uniqueness_of(:repo_id).scoped_to(:language_id) }
   end
 end
