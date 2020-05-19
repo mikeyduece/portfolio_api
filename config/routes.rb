20b791638a344cac85e0d4a60748239bcb5e2613
Mike Heft <mikeheft@gmail.com>
2020-05-19 13:57:42 -0600

    Add subdomain routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,16 @@ Rails.application.routes.draw do
       resource :contact, module: :contact, only: :create
     end
   end
+
+  constraints(subdomain: "api") do
+    constraints(subdomain: "v1") do
+      scope module: :api do
+        scope module: :v1 do
+          resources :languages, module: :languages, only: :index
+          resource :contact, module: :contact, only: :create
+        end
+      end
+    end
+  end
+  
 end
